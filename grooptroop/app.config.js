beca08a6012f8b0ea08b3571ba8d3a654f372a79
Nick Sanders <nick.sanders.a@gmail.com>
2025-05-02 18:06:50 -0700

    updating

--- grooptroop/app.config.js
+++ grooptroop/app.config.js
@@ -3,13 +3,46 @@ import 'dotenv/config';
 export default ({ config }) => {
   return {
     ...config,
+    name: "groopTroop",
+    slug: "grooptroop",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/GTlogo.png",
+    splash: {
+      image: "./assets/GTlogo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.nas4146.grooptroop",
+      buildNumber: "1.0.0"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+      package: "com.nas4146.grooptroop",
+      versionCode: 1
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
     expo: {
       ...config.expo,
       plugins: [
         ...(config.expo?.plugins || []),
-        "expo-secure-store"
+        "expo-secure-store",
+        "expo-web-browser" // Add this line for web browser support
       ],
-      jsEngine: "hermes", // Explicitly set jsEngine to hermes
+      jsEngine: "hermes",
       extra: {
         ...config.extra,
         firebaseApiKey: process.env.FIREBASE_API_KEY,
@@ -18,6 +51,10 @@ export default ({ config }) => {
         firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
         firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         firebaseAppId: process.env.FIREBASE_APP_ID,
+        // Add OAuth client IDs for Google authentication
+        googleClientId: process.env.GOOGLE_CLIENT_ID,
+        googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+        googleAndroidClientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
         eas: {
           projectId: "aa4a7513-c314-4aae-9caf-4287b478b827"
         }
