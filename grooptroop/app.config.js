9bf411bf577a10f8b580305769aa68a3829ee0c8
Nick Sanders <nick.sanders.a@gmail.com>
2025-05-02 18:11:51 -0700

    update

--- grooptroop/app.config.js
+++ grooptroop/app.config.js
@@ -19,6 +19,24 @@ export default ({ config }) => {
     assetBundlePatterns: [
       "**/*"
     ],
+    plugins: [
+      "expo-secure-store"
+    ],
+    jsEngine: "hermes",
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID, 
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      googleClientId: process.env.GOOGLE_CLIENT_ID,
+      googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+      googleAndroidClientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
+      eas: {
+        projectId: "aa4a7513-c314-4aae-9caf-4287b478b827"
+      }
+    },
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.nas4146.grooptroop",
@@ -34,30 +52,6 @@ export default ({ config }) => {
     },
     web: {
       favicon: "./assets/favicon.png"
-    },
-    expo: {
-      ...config.expo,
-      plugins: [
-        ...(config.expo?.plugins || []),
-        "expo-secure-store",
-      ],
-      jsEngine: "hermes",
-      extra: {
-        ...config.extra,
-        firebaseApiKey: process.env.FIREBASE_API_KEY,
-        firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        firebaseProjectId: process.env.FIREBASE_PROJECT_ID, 
-        firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        firebaseAppId: process.env.FIREBASE_APP_ID,
-        // Add OAuth client IDs for Google authentication
-        googleClientId: process.env.GOOGLE_CLIENT_ID,
-        googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
-        googleAndroidClientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
-        eas: {
-          projectId: "aa4a7513-c314-4aae-9caf-4287b478b827"
-        }
-      }
     }
   };
 };
