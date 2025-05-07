c77653f103f8e18324d5f3253f6801ef0a7f0088
Nick Sanders <nick.sanders.a@gmail.com>
2025-05-06 17:37:51 -0700

    fix

---
+++ grooptroop/metro.config.js
@@ -0,0 +1,7 @@
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Customize the config if needed
+module.exports = config;
